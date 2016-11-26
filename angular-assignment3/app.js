3d2bc6003d2838f8884f58e138cde19edb3a2023
Paul Trembath <paultrembath1@gmail.com>
2016-11-26 20:22:12 +0000

    Candidate solution for Angular assignment 3

--- angular-assignment3/app.js
+++ angular-assignment3/app.js
@@ -11,11 +11,11 @@ function FoundItemsDirective() {
   var ddo = {
     templateUrl: 'foundItems.html',
     scope: {
-      foundItems: '<',
+      itemList: '<',
       onRemove: '&'
     },
     controller: FoundItemsDirectiveController,
-    controllerAs: 'items',
+    controllerAs: 'fidc',
     bindToController: true
   };
 
@@ -25,63 +25,81 @@ function FoundItemsDirective() {
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var nidc = this;
-  nidc.found = [];
   nidc.searchTerm = "";
+  nidc.haveWeSearchedYet = false;
 
   nidc.btnNarrowIt = function() {
-    console.log("btnNarrowIt()");
-    nidc.found = MenuSearchService.getMatchedMenuItems(nidc.searchTerm);
+    var promise = MenuSearchService.getMatchedMenuItems(nidc.searchTerm);
+    promise.then( function(result) {
+      nidc.found = result.foundItems;
+    })
+    .catch(function (error) {
+      console.log("Search failed: ", error);
+    });
   }
 
   nidc.onRemove = function(index) {
-    console.log("onRemove(index=", index, ")");
+    nidc.found.splice(index, 1);
   }
 }
 
 function FoundItemsDirectiveController() {
-  var foundItems = this;
+  var fidc = this;
   return false;
 }
 
-MenuSearchService.$inject = ['$http', 'ApiPath'];
-function MenuSearchService($http, ApiPath) {
+MenuSearchService.$inject = ['$q', '$http', 'ApiPath'];
+function MenuSearchService($q, $http, ApiPath) {
   var service = this;
 
+  // asynchronously retrieve menu items, returning a promise
   service.getMatchedMenuItems = function (searchTerm) {
-    console.log("$http call, searchTerm=", searchTerm);
+    var deferred = $q.defer();
+    var searchResult = {
+      foundItems: []
+    };
 
-    var searchTermLower = searchTerm.toLowerCase();
-    return $http({
-      method: "GET",
-      url: (ApiPath)
-    })
-    .then(function (result) {
-      console.log("$http result");
-      // process result and only keep items that match
-      var foundItems = [];
+    if (! searchTerm.trim().length) {
+      // return no items when search term is empty or white space
+      deferred.resolve(searchResult);
+    }
+    else {
+      $http({
+        method: "GET",
+        url: (ApiPath)
+      })
+      .then(function (result) {
+        // process result and only keep items that match
+        var searchTermLower = searchTerm.toLowerCase();
+        var foundItems = [];
 
-      for (var index in result.data.menu_items) {
-        var item = result.data.menu_items[index];
-        // description
-        // id
-        // large_portion_name
-        // name
-        // price_large
-        // price_small
-        // short_name
-        // small_portion_name
-        if (item.description.toLowerCase().indexOf(searchTermLower) != -1) {
-          foundItems.push(item);
+        for (var index in result.data.menu_items) {
+          var item = result.data.menu_items[index];
+          // description
+          // id
+          // large_portion_name
+          // name
+          // price_large
+          // price_small
+          // short_name
+          // small_portion_name
+          if (item.description.toLowerCase().indexOf(searchTermLower) != -1) {
+            foundItems.push(item);
+          }
         }
-      }
 
-      // return processed items
-      return foundItems;
-    })
-    .catch(function (error) {
-      console.log("Something went terribly wrong.");
-      console.log("Error object:", error);
-    });
+        // return processed items
+        searchResult.foundItems = foundItems;
+        deferred.resolve(searchResult);
+      })
+      .catch(function (error) {
+        console.log("Something went wrong in search service.");
+        console.log("Error object:", error);
+        deferred.reject("The search failed");
+      });
+    }
+
+    return deferred.promise;
   }
 }
 
