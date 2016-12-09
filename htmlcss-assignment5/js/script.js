42361963f3616df406bbc7056f608f2a9a800324
Paul Trembath <paultrembath1@gmail.com>
2016-12-09 22:35:29 +0000

    Assignment 5, remove log messages

--- htmlcss-assignment5/js/script.js
+++ htmlcss-assignment5/js/script.js
@@ -102,7 +102,6 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-      console.log("Random short_name is: ", chosenCategoryShortName);
 
       // XTODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -120,7 +119,6 @@ function buildAndShowHomeHTML (categories) {
       // XTODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      console.log("Insert #main-content:", homeHtmlToInsertIntoMainPage);
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
     },
